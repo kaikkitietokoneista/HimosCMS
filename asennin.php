926962a435152c7a8b2c7f8cdeff942676be41e3
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2019-12-30 23:01:58 +0200

    Update asennin.php

--- asennin.php
+++ asennin.php
@@ -1,22 +1,51 @@
 <?php
 
   if ($_POST["kayttajanimi"] != "") {
-    $asetukset = new stdClass();
-    $asetukset->nimi = $_POST["sivunnimi"];
-    $suolattupassu = password_hash($_POST["passu"], PASSWORD_DEFAULT);
-    $asetukset->hallintasalasana = $suolattupassu;
-    $asetukset->hallintanimi = $_POST["kayttajanimi"];
+    if (!is_dir("osat") || !is_dir("sivut")) {
+      $asennushakemisto = $_POST["asennushakemisto"];
+      if (!is_dir($asennushakemisto)) {
+        mkdir($asennushakemisto);
+        $loki = $loki . "<br>Tehdään hakemistoa.";
+      } else {
+        $loki = $loki . "<br>Hakemisto olemassa. Asennetaan siihen.";
+      }
 
-    $json = json_encode($asetukset);
+      chdir($asennushakemisto);
 
-    $jsontiedosto = fopen("asetukset.json", "w") or die("Kriittinen virhe! Korjaus: käytä uudempaa asennusohjelmaa tai ota yhteyttä HimosCMS:n ylläpitoon.");
-    fwrite($jsontiedosto, $json);
-  }
-  $asennushakemisto = $_POST["asennushakemisto"];
-  if (!is_dir($asennushakemisto)) {
-    mkdir($asennushakemisto);
-  } else {
-    
+      file_put_contents("asennus.zip", fopen("https://himoscms.r.takanen.eu/HimosCMS_viimeisin.zip", 'r'));
+
+      $zip = new ZipArchive;
+      $res = $zip->open('asennus.zip');
+      if ($res === TRUE) {
+        $zip->extractTo($asennushakemisto);
+        $zip->close();
+      }
+      unlink("asennus.zip");
+
+      //Vasta vikana, jotta ylikirjoittaa zip-paketista tulleen asetukset.json:in
+      $asetukset = new stdClass();
+      $asetukset->nimi = $_POST["sivunnimi"];
+      $suolattupassu = password_hash($_POST["passu"], PASSWORD_DEFAULT);
+      $asetukset->hallintasalasana = $suolattupassu;
+      $asetukset->hallintanimi = $_POST["kayttajanimi"];
+
+      $json = json_encode($asetukset);
+
+      $jsontiedosto = fopen("asetukset.json", "w") or die("Kriittinen virhe! Korjaus: käytä uudempaa asennusohjelmaa tai ota yhteyttä HimosCMS:n ylläpitoon.");
+      fwrite($jsontiedosto, $json);
+    } else {
+      //Eli osat ja sivut hakemistot ovat olemassa niin resetoi vain passu
+      $asetukset = new stdClass();
+      $asetukset->nimi = $_POST["sivunnimi"];
+      $suolattupassu = password_hash($_POST["passu"], PASSWORD_DEFAULT);
+      $asetukset->hallintasalasana = $suolattupassu;
+      $asetukset->hallintanimi = $_POST["kayttajanimi"];
+
+      $json = json_encode($asetukset);
+
+      $jsontiedosto = fopen("asetukset.json", "w") or die("Kriittinen virhe! Korjaus: käytä uudempaa asennusohjelmaa tai ota yhteyttä HimosCMS:n ylläpitoon.");
+      fwrite($jsontiedosto, $json);
+    }
   }
 
  ?>
@@ -40,7 +69,7 @@
             <input type="text" class="form-control" name="kayttajanimi" placeholder="Käyttäjänimi..." name="passu" id="inputtikayttajanimi" required>
           </div>
           <div class="form-group col-md-6">
-            <label for="inputtipassu">Uusi salasana (unohtuneen <a href="#" data-toggle="popover" title="Salasanan resetointi" data-content="Voit käyttää tätä asenninta olemassaolevan HimosCMS-asennuksen salasanan resetoimiseen. Säilytät varmuuskopiosi sivusi ja etusivusi osat silloin.">resetoiminen</a>):</label>
+            <label for="inputtipassu">Uusi salasana (unohtuneen <a href="#" data-toggle="popover" title="Salasanan palauttaminen" data-content="Voit käyttää tätä asenninta olemassaolevan HimosCMS-asennuksen salasanan palauttamiseen. Säilytät varmuuskopiosi sivusi ja etusivusi osat silloin.">palauttaminen</a>):</label>
             <input type="password" class="form-control" name="passu" placeholder="Uusi salasana..." name="passu" id="inputtipassu" required>
           </div>
           <div class="form-group col-md-12">
