ad47823e3b6fea365a3ba62f04c86f9229983aac
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2020-01-17 17:36:12 +0200

    Päivitys

--- index.php
+++ index.php
@@ -3,11 +3,6 @@ include 'asetukset.php';
 require 'funktiot.php';
 
  ?>
-<style type="text/css">
-  <?php
-    //Custom CSS
-   ?>
-</style>
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
